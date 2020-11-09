0faba6ea318646ad363ee2734c837a241982a4f4
JenyaDev <yyakovliev02@gmail.com>
2020-11-09 22:13:11 +0200

    Fixed lab

--- second/first.c
+++ second/first.c
@@ -3,25 +3,23 @@
 
 int main() {
     int n;
-    int counter = 0;
     printf("Enter your number: ");
     scanf("%d", &n);
-    
-    if (n > 0) {
-        double sumMult = 1;
-        for (int i = 1; i <= n; i++) {
-            double sumAdd = 0;
-            for (int j = 1; j <= i; j++) {
-                sumAdd += sin(j) + 1;
-                counter += 3; // Sine and addition
-            }
-            sumMult *= sumAdd/i*(i+1);
-            counter += 4; // 2xMultiplication, addition and division
-        }
 
-        printf("Result: %f\n", sumMult);
-        printf("Number of operations: %d\n", counter);
+    double sumMult = 1;
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+        double sumAdd = 0;
+        for (int j = 1; j <= i; j++) {
+            sumAdd += sin(j) + 1;
+            counter += 3;
+        }
+        sumMult *= sumAdd/(i*(i+1));
+        counter += 4;
     }
+
+    printf("Result: %f\n", sumMult);
+    printf("Number of operations: %d", counter);
 
     return 0;
 }
--- second/second.c
+++ second/second.c
@@ -3,22 +3,20 @@
 
 int main() {
     int n;
-    int counter = 0;
     printf("Enter your number: ");
     scanf("%d", &n);
-    
-    if (n > 0) {
-        double sumMult = 1;
-        for (int i = 1; i <= n; i++) {
-            double sumAdd = 0;
-            sumAdd += sin(i) + 1;
-            sumMult *= sumAdd/i*(i+1);
-            counter += 7; // Sine, division, two additions and multiplication
-        }
 
-        printf("Result: %f\n", sumMult);
-        printf("Number of operations: %d\n", counter);
+    double sumMult = 1;
+    double sumAdd = 0;
+    int counter = 0;
+    for (int i = 1; i <= n; i++) {
+        sumAdd += sin(i) + 1;
+        sumMult *= sumAdd/(i*(i+1));
+        counter += 7;
     }
+
+    printf("Result: %f\n", sumMult);
+    printf("Number of operations: %d", counter);
 
     return 0;
 }
